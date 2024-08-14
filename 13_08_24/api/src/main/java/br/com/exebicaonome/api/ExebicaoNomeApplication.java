aa61e34c373849545b759259ebfdca266f31210c
d4nkali <133923419+d4nkali@users.noreply.github.com>
2024-08-14 11:50:11 -0300

    Conteudo do dia 13_08_24

---
+++ 13_08_24/api/src/main/java/br/com/exebicaonome/api/ExebicaoNomeApplication.java
@@ -0,0 +1,13 @@
+package br.com.exebicaonome.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExebicaoNomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExebicaoNomeApplication.class, args);
+	}
+
+}
---
+++ 13_08_24/api/src/test/java/br/com/exebicaonome/api/ExebicaoNomeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.exebicaonome.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExebicaoNomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
