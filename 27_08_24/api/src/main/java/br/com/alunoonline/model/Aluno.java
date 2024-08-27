26ed539b0d3bb34788e7ea071f8317b78c81e147
d4nkali <133923419+d4nkali@users.noreply.github.com>
2024-08-27 20:40:58 -0300

    Add api project Aluno Online

---
+++ 27_08_24/api/src/main/java/br/com/alunoonline/model/Aluno.java
@@ -0,0 +1,14 @@
+package br.com.alunoonline.model;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+@Entity
+public class Aluno {
+
+
+
+}
