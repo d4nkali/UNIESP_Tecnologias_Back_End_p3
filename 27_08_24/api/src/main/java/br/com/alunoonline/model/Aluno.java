9c528c101664e16a10d4753d03b479db71ed5a7c
d4nkali <133923419+d4nkali@users.noreply.github.com>
2024-08-27 21:07:09 -0300

    Add api project Aluno Online

---
+++ 27_08_24/api/src/main/java/br/com/alunoonline/model/Aluno.java
@@ -0,0 +1,28 @@
+package br.com.alunoonline.model;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+public class Aluno implements Serializable{
+
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id
+	private Long id;
+
+	private String nome;
+
+	private String cpf;
+
+}
