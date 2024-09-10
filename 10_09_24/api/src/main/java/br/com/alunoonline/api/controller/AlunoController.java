02a64053dd98fb517c53f18390aa31720425cf56
d4nkali <133923419+d4nkali@users.noreply.github.com>
2024-09-10 20:36:18 -0300

    Adicionando endpoints buscar alunos

--- 10_09_24/api/src/main/java/br/com/alunoonline/api/controller/AlunoController.java
+++ 10_09_24/api/src/main/java/br/com/alunoonline/api/controller/AlunoController.java
@@ -2,9 +2,14 @@ package br.com.alunoonline.api.controller;
 
 import br.com.alunoonline.api.model.Aluno;
 import br.com.alunoonline.api.service.AlunoService;
+
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 @RequestMapping("/alunos")
@@ -12,13 +17,29 @@ public class AlunoController {
 
 	@Autowired
 	AlunoService alunoService;
-	
+
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public void criarAluno(@RequestBody Aluno aluno) {
-		
+
 		alunoService.criarAluno(aluno);
-		
+
 	}
-	
+
+	@GetMapping
+	@ResponseStatus(HttpStatus.OK)
+	public List<Aluno> listarTodosAlunos() {
+
+		return alunoService.listarTodosAlunos();
+
+	}
+
+	@GetMapping("/{id}")
+	@ResponseStatus(HttpStatus.OK)
+	public Optional<Aluno> buscarAlunoPorId(@PathVariable Long id) {
+
+		return alunoService.buscarAlunoPorId(id);
+
+	}
+
 }
--- 10_09_24/api/src/main/java/br/com/alunoonline/api/service/AlunoService.java
+++ 10_09_24/api/src/main/java/br/com/alunoonline/api/service/AlunoService.java
@@ -1,8 +1,13 @@
 package br.com.alunoonline.api.service;
 
 import br.com.alunoonline.api.model.Aluno;
+
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 
 import br.com.alunoonline.api.repository.AlunoRepository;
 
@@ -13,9 +18,21 @@ public class AlunoService {
 	AlunoRepository alunoRepository;
 
 	public void criarAluno(Aluno aluno) {
-		
+
 		alunoRepository.save(aluno);
-		
+
 	}
-	
+
+	public List<Aluno> listarTodosAlunos() {
+
+		return alunoRepository.findAll();
+
+	}
+
+	public Optional<Aluno> buscarAlunoPorId(Long id) {
+
+		return alunoRepository.findById(id);
+
+	}
+
 }
