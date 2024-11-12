b27ba39e2d7c5486d077b3cfed012f1384c2f4fd
d4nkali <133923419+d4nkali@users.noreply.github.com>
2024-11-12 16:55:13 -0300

    Adicionando atualizar notas no controller de MatriculaAluno

--- 05_11_24/api/src/main/java/br/com/alunoonline/api/controller/MatriculaAlunoController.java
+++ 05_11_24/api/src/main/java/br/com/alunoonline/api/controller/MatriculaAlunoController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.alunoonline.api.dtos.AtualizarNotasRequest;
 import br.com.alunoonline.api.model.MatriculaAluno;
 import br.com.alunoonline.api.service.MatriculaAlunoService;
 
@@ -33,6 +34,14 @@ public class MatriculaAlunoController {
 	public void trancarMatricula(@PathVariable Long id) {
 
 		matriculaAlunoService.trancarMatricula(id);
+
+	}
+
+	@PatchMapping("/atualiza-notas/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void atualizarNotas(@RequestBody AtualizarNotasRequest atualizarNotasRequest, @PathVariable Long id){
+
+		matriculaAlunoService.atualizarNotas(id, atualizarNotasRequest);
 
 	}
 
