57202bd638c72edb51cb35d8c311400147d5e7e1
d4nkali <133923419+d4nkali@users.noreply.github.com>
2024-11-23 17:50:01 -0300

    Adicionando controller para o historico

--- 19_11_24/api/src/main/java/br/com/alunoonline/api/controller/MatriculaAlunoController.java
+++ 19_11_24/api/src/main/java/br/com/alunoonline/api/controller/MatriculaAlunoController.java
@@ -2,6 +2,7 @@ package br.com.alunoonline.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.alunoonline.api.dtos.AtualizarNotasRequest;
+import br.com.alunoonline.api.dtos.HistoricoAlunoResponse;
 import br.com.alunoonline.api.model.MatriculaAluno;
 import br.com.alunoonline.api.service.MatriculaAlunoService;
 
@@ -43,6 +45,14 @@ public class MatriculaAlunoController {
 
 		matriculaAlunoService.atualizarNotas(id, atualizarNotasRequest);
 
+	}
+
+	@GetMapping("/emitir-historico/{alunoId}")
+    @ResponseStatus(HttpStatus.OK)
+    public HistoricoAlunoResponse emitirHistorico(@PathVariable Long alunoId) {
+    
+		return matriculaAlunoService.emitirHistorico(alunoId);
+    
 	}
 
 }
